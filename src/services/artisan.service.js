873661bd9187f7cb84f5615b70b763e752870816
mohamed <mohamed.ing2@gmail.com>
2026-01-13 23:48:47 +0100

    fix 8: api_url

--- src/services/artisan.service.js
+++ src/services/artisan.service.js
@@ -1,5 +1,5 @@
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 export async function getArtisansByCategorie(categorie) {
   try {
     const res = await fetch(
