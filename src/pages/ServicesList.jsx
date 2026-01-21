a287628be3befdd1b4e5dd98b97db809b457fd64
mohamed <mohamed.ing2@gmail.com>
2026-01-21 11:46:29 +0100

    mise à jour pages

--- src/pages/ServicesList.jsx
+++ src/pages/ServicesList.jsx
@@ -78,7 +78,7 @@ export default function Services() {
         </p>
       </nav>
 
-      <h1 className="fw-bold mb-4">Trouver un prestataire de services</h1>
+      <h1 className="fw-bold text-blue mb-4">Trouver un prestataire de services</h1>
 
       <div className="row">
         <aside className="col-md-3 mb-4">
