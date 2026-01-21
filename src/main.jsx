63138ec584b39a5cdbbc00b688d0de4dc47eb0b2
mohamed <mohamed.ing2@gmail.com>
2026-01-21 10:42:47 +0100

    mise à jour styles scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "./styles/custom.css";
+
+import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
